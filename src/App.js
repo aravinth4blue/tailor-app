7d4930b2b29a5afe7372889e7a8d4c932293105b
Aravinth <aravinth.krishnamoorthi@gmail.com>
2020-07-21 07:56:17 +0530

    Change in front screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import parse from 'html-react-parser';
+
 
 class App extends React.Component {
-
+   constructor(props){
+      super(props);
+      this.state = {
+         customDiv: ['<div class="form-row"> <div class="form-group col-md-2"> <label for="">Material</label> <select id="inputState" class="form-control" name="material"> <option selected>Choose...</option> <option>Saree</option> <option >Chudi</option> <option>Lining Blouse</option> <option>Blouse</option> <option>Sattai Button</option> <option>Pavadai Belt</option> </select> </div> <div class="form-group col-md-2"> <label for="">Color</label> <select id="inputState" class="form-control" name="status"> <option selected>Choose...</option> <option>Blue</option> <option>Yellow</option> <option>Red</option> </select> </div> <div class="form-group col-md-2"> <label for="">Qty</label> <select id="inputState" class="form-control" name="qty"> <option selected>Choose...</option> <option>1</option> <option>2</option> <option>3</option> <option>4</option> <option>5</option> <option>6</option> <option>7</option> <option>8</option> <option>9</option> <option>10</option> </select> </div> <div class="form-group col-md-1"> <label for="">Stiched</label> <input type="text" class="form-control" name="stiched_by"/> </div> <div class="form-group col-md-1"> <label for="">Cutted</label> <input type="text" class="form-control" name="cutted_by"/> </div> <div class="form-group col-md-1"> <label for="">Price</label> <input type="text" class="form-control" name="price"/> </div> <div class="form-group col-md-2"> <label for="">Status</label> <select id="inputState" class="form-control" name="status"> <option selected>Choose...</option> <option>Paid</option> <option>Un Paid</option> <option>Partial</option> </select> </div>']     // set initial state with one div
+      }
+      console.log(this.state.customDiv)
+      this.addRow = this.addRow.bind(this);
+      
+    }
+   addRow() {
+      console.log("Add button clicked");
+      
+      let cDivs = [...this.state.customDiv];
+      cDivs.push('<div class="form-row"> <div class="form-group col-md-2"> <label for="">Material</label> <select id="inputState" class="form-control" name="material"> <option selected>Choose...</option> <option>Saree</option> <option >Chudi</option> <option>Lining Blouse</option> <option>Blouse</option> <option>Sattai Button</option> <option>Pavadai Belt</option> </select> </div> <div class="form-group col-md-2"> <label for="">Color</label> <select id="inputState" class="form-control" name="status"> <option selected>Choose...</option> <option>Blue</option> <option>Yellow</option> <option>Red</option> </select> </div> <div class="form-group col-md-2"> <label for="">Qty</label> <select id="inputState" class="form-control" name="qty"> <option selected>Choose...</option> <option>1</option> <option>2</option> <option>3</option> <option>4</option> <option>5</option> <option>6</option> <option>7</option> <option>8</option> <option>9</option> <option>10</option> </select> </div> <div class="form-group col-md-1"> <label for="">Stiched</label> <input type="text" class="form-control" name="stiched_by"/> </div> <div class="form-group col-md-1"> <label for="">Cutted</label> <input type="text" class="form-control" name="cutted_by"/> </div> <div class="form-group col-md-1"> <label for="">Price</label> <input type="text" class="form-control" name="price"/> </div> <div class="form-group col-md-2"> <label for="">Status</label> <select id="inputState" class="form-control" name="status"> <option selected>Choose...</option> <option>Paid</option> <option>Un Paid</option> <option>Partial</option> </select> </div>')
+      this.setState({customDiv: cDivs })
+      console.log(this.state.customDiv)
+      console.log(typeof this.state)
+   }
   render(){
+   
     
     return (
         <React.Fragment>
@@ -44,69 +64,13 @@ class App extends React.Component {
                      <input type="text" class="form-control" name="cus_name"/>
                   </div>
                </div>
-               <div class="form-row">
+               {parse(String(this.state.customDiv))}
+                  
                   <div class="form-group col-md-2">
-                     <label for="">Material</label>
-                     <select id="inputState" class="form-control" name="meterial">
-                        <option selected>Choose...</option>
-                        <option>Saree</option>
-                        <option >Chudi</option>
-                        <option>Lining Blouse</option>
-                        <option>Blouse</option>
-                        <option>Sattai Button</option>
-                        <option>Pavadai Belt</option>
-                     </select>
+                        <button type="button"
+             className="btn btn-primary custom-add-button add-row"
+             onClick={this.addRow}><i class="icon-plus-sign"></i></button>
                   </div>
-                  <div class="form-group col-md-2">
-                     <label for="">Color</label>
-                     <select id="inputState" class="form-control" name="status">
-                        <option selected>Choose...</option>
-                        <option>Blue</option>
-                        <option>Yellow</option>
-                        <option>Red</option>
-                     </select>
-                  </div>
-                  <div class="form-group col-md-2">
-                     <label for="">Qty</label>
-                     <select id="inputState" class="form-control" name="qty">
-                        <option selected>Choose...</option>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                        <option>6</option>
-                        <option>7</option>
-                        <option>8</option>
-                        <option>9</option>
-                        <option>10</option>
-                     </select>
-                  </div>
-               
-                  <div class="form-group col-md-1">
-                     <label for="">Stiched</label>
-                     <input type="text" class="form-control" name="stiched_by"/>
-                  </div>
-                  <div class="form-group col-md-1">
-                     <label for="">Cutted</label>
-                     <input type="text" class="form-control" name="cutted_by"/>
-                  </div>
-               
-                  <div class="form-group col-md-1">
-                     <label for="">Price</label>
-                     <input type="text" class="form-control" name="price"/>
-                  </div>
-                  <div class="form-group col-md-2">
-                     <label for="">Status</label>
-                     <select id="inputState" class="form-control" name="status">
-                        <option selected>Choose...</option>
-                        <option>Paid</option>
-                        <option>Un Paid</option>
-                        <option>Partial</option>
-                     </select>
-                  </div>
-                  <i class="icon-plus-sign"></i>
-               </div>
                <div class="form-group col-md-12">
                   <button type="submit" class="btn btn-primary" name="submit">Submit</button>
                </div>
@@ -114,7 +78,14 @@ class App extends React.Component {
          </div>
       </div>
       </div>
+      <div>
+        
+        {/* .map((cdiv,i) => {
+          <div className="expense-block" key={cdiv}>></div>
+        }) */}
+        </div>
       </React.Fragment>
+      
     );
   }
 
